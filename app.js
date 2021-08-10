f73d256ba218759b688b9fc7c3b7cfff82e239b9
Samuel Anumudu <samuelmaize55@yahoo.com>
2021-08-10 02:12:30 +0200

    Using Body Parser to Parse POST

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
   // Get live data using an API
+  const query = req.body.cityName;
+  const apiKey = "a63b6d1a225a7a81f30f0b620673fd93";
+  const unit = "metric";
+
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Kigali&appid=a63b6d1a225a7a81f30f0b620673fd93&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
 
   // Make a http get request to get the data as a JSON format
   https.get(url, (response) => {
@@ -26,10 +42,10 @@ app.get("/", (req, res) => {
 
       //   Send the request to the browser using the HTML you want to write
 
-      res.write("<p>The weather in Kigali is " + weatherDesc + "</p>");
+      res.write("<p>The weather in " + query + " is " + weatherDesc + "</p>");
 
       res.write(
-        "<h1>The weather in Kigali is " + temp + "degrees Celcius.</h1>"
+        "<h1>The weather in " + query + " is " + temp + "degrees Celcius.</h1>"
       );
 
       res.write("<img src=" + imageURL + " />");
